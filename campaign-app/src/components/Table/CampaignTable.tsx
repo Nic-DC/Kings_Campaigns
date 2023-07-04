f00832590c698cacf6982ccdeaf07c13e03711ff
Nic <nic.dan.costea@gmail.com>
2023-07-04 19:07:16 +0200

    drawer

--- campaign-app/src/components/Table/CampaignTable.tsx
+++ campaign-app/src/components/Table/CampaignTable.tsx
@@ -1,4 +1,4 @@
-import React, { useState, ChangeEvent } from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { selectFilteredCampaigns } from "../../redux/campaigns/selectors";
 import CampaignRow from "./CampaignRow";
@@ -16,6 +16,7 @@ import FilterListIcon from "@mui/icons-material/FilterList";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import TablePaginationDemo from "./TablePagination";
 import campaigns from "../../data/campaigns";
+import FilterDrawer from "./FilterDrawer";
 
 interface CampaignTableProps {
   searchValue: string;
@@ -25,6 +26,7 @@ const CampaignTable: React.FC<CampaignTableProps> = ({ searchValue }) => {
   const [rows, setRows] = useState<Campaign[]>([]);
   const [currentPage, setCurrentPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [filterDrawerOpen, setFilterDrawerOpen] = useState(false);
 
   const addRows = (newRows: Campaign[]) => {
     setRows((prevRows) => [...prevRows, ...newRows]);
@@ -39,12 +41,20 @@ const CampaignTable: React.FC<CampaignTableProps> = ({ searchValue }) => {
     setCurrentPage(0);
   };
 
+  const handleFilterClick = () => {
+    setFilterDrawerOpen(true);
+  };
+
   // Filter campaigns based on search value
   const filteredCampaigns = campaigns.filter((campaign) =>
     campaign.name.toLowerCase().includes(searchValue.toLowerCase())
   );
 
   const paginatedCampaigns = filteredCampaigns.slice(currentPage * rowsPerPage, (currentPage + 1) * rowsPerPage);
+
+  const handleFilterDrawerClose = () => {
+    setFilterDrawerOpen(false);
+  };
 
   return (
     <Box m={3}>
@@ -53,7 +63,7 @@ const CampaignTable: React.FC<CampaignTableProps> = ({ searchValue }) => {
           <TableHead style={{ color: alpha("#000", 0.3), fontSize: "150%" }}>
             <TableRow>
               <TableCell>
-                <FilterListIcon />
+                <FilterListIcon onClick={handleFilterClick} />
               </TableCell>
               <TableCell></TableCell>
               <TableCell></TableCell>
@@ -93,8 +103,110 @@ const CampaignTable: React.FC<CampaignTableProps> = ({ searchValue }) => {
         totalPages={Math.ceil(filteredCampaigns.length / rowsPerPage)}
         count={filteredCampaigns.length}
       />
+      <FilterDrawer open={filterDrawerOpen} onClose={handleFilterDrawerClose} />
     </Box>
   );
 };
 
 export default CampaignTable;
+
+// import React, { useState, ChangeEvent } from "react";
+// import { useSelector } from "react-redux";
+// import { selectFilteredCampaigns } from "../../redux/campaigns/selectors";
+// import CampaignRow from "./CampaignRow";
+// import Table from "@mui/material/Table";
+// import TableBody from "@mui/material/TableBody";
+// import TableCell from "@mui/material/TableCell";
+// import TableContainer from "@mui/material/TableContainer";
+// import TableHead from "@mui/material/TableHead";
+// import TableRow from "@mui/material/TableRow";
+// import Paper from "@mui/material/Paper";
+// import Campaign from "../../types";
+// import Box from "@mui/material/Box";
+// import { alpha } from "@mui/material/styles";
+// import FilterListIcon from "@mui/icons-material/FilterList";
+// import MoreVertIcon from "@mui/icons-material/MoreVert";
+// import TablePaginationDemo from "./TablePagination";
+// import campaigns from "../../data/campaigns";
+
+// interface CampaignTableProps {
+//   searchValue: string;
+// }
+
+// const CampaignTable: React.FC<CampaignTableProps> = ({ searchValue }) => {
+//   const [rows, setRows] = useState<Campaign[]>([]);
+//   const [currentPage, setCurrentPage] = useState(0);
+//   const [rowsPerPage, setRowsPerPage] = useState(10);
+
+//   const addRows = (newRows: Campaign[]) => {
+//     setRows((prevRows) => [...prevRows, ...newRows]);
+//   };
+
+//   const handleChangePage = (event: React.MouseEvent<HTMLButtonElement> | null, newPage: number) => {
+//     setCurrentPage(newPage);
+//   };
+
+//   const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+//     setRowsPerPage(parseInt(event.target.value, 10));
+//     setCurrentPage(0);
+//   };
+
+//   // Filter campaigns based on search value
+//   const filteredCampaigns = campaigns.filter((campaign) =>
+//     campaign.name.toLowerCase().includes(searchValue.toLowerCase())
+//   );
+
+//   const paginatedCampaigns = filteredCampaigns.slice(currentPage * rowsPerPage, (currentPage + 1) * rowsPerPage);
+
+//   return (
+//     <Box m={3}>
+//       <TableContainer component={Paper}>
+//         <Table>
+//           <TableHead style={{ color: alpha("#000", 0.3), fontSize: "150%" }}>
+//             <TableRow>
+//               <TableCell>
+//                 <FilterListIcon />
+//               </TableCell>
+//               <TableCell></TableCell>
+//               <TableCell></TableCell>
+//               <TableCell></TableCell>
+//               <TableCell align="right">
+//                 <MoreVertIcon />
+//               </TableCell>
+//             </TableRow>
+//           </TableHead>
+//           <TableHead>
+//             <TableRow>
+//               <TableCell style={{ fontWeight: "bold", color: "grey", fontSize: "0.75rem" }}>Name</TableCell>
+//               <TableCell style={{ fontWeight: "bold", color: "grey", fontSize: "0.75rem" }}>Status</TableCell>
+//               <TableCell align="right" style={{ fontWeight: "bold", color: "grey", fontSize: "0.75rem" }}>
+//                 Start Date
+//               </TableCell>
+//               <TableCell align="right" style={{ fontWeight: "bold", color: "grey", fontSize: "0.75rem" }}>
+//                 End Date
+//               </TableCell>
+//               <TableCell align="right" style={{ fontWeight: "bold", color: "grey", fontSize: "0.75rem" }}>
+//                 Budget
+//               </TableCell>
+//             </TableRow>
+//           </TableHead>
+//           <TableBody>
+//             {paginatedCampaigns.map((campaign) => (
+//               <CampaignRow key={campaign.id} campaign={campaign} />
+//             ))}
+//           </TableBody>
+//         </Table>
+//       </TableContainer>
+//       <TablePaginationDemo
+//         currentPage={currentPage}
+//         rowsPerPage={rowsPerPage}
+//         handleChangePage={handleChangePage}
+//         handleChangeRowsPerPage={handleChangeRowsPerPage}
+//         totalPages={Math.ceil(filteredCampaigns.length / rowsPerPage)}
+//         count={filteredCampaigns.length}
+//       />
+//     </Box>
+//   );
+// };
+
+// export default CampaignTable;
--- campaign-app/src/components/Table/FilterDrawer.tsx
+++ campaign-app/src/components/Table/FilterDrawer.tsx
@@ -1,9 +1,59 @@
-// src/components/FilterDrawer.tsx
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+import FilterListIcon from "@mui/icons-material/FilterList";
 
-import React from "react";
+type Anchor = "right";
 
-const FilterDrawer: React.FC = () => {
-  return <div>{/* Date range picker will go here */}</div>;
+interface FilterDrawerProps {
+  open: boolean;
+  onClose: () => void;
+}
+
+const FilterDrawer: React.FC<FilterDrawerProps> = ({ open, onClose }) => {
+  const anchor: Anchor = "right";
+
+  const list = (anchor: Anchor) => (
+    <Box sx={{ width: 250 }} role="presentation" onClick={onClose} onKeyDown={onClose}>
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <div>
+      <Drawer anchor={anchor} open={open} onClose={onClose}>
+        {list(anchor)}
+      </Drawer>
+    </div>
+  );
 };
 
 export default FilterDrawer;
